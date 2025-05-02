d755cf836bcaf87133f1fb0f2b6c8a853d6a2f2f
Mari Myrmoen <mari.myr@gmail.com>
2025-05-02 17:14:32 +0200

    Added searchbar with input and results

--- src/components/eventslist/EventsList.jsx
+++ src/components/eventslist/EventsList.jsx
@@ -1,16 +1,7 @@
 import './eventslist.css';
 import EventItem from '../eventitem/EventItem';
-import useFetch from "../../hooks/useFetch";
 
-function EventsList() {
-  const { data : events, isLoading, isError } = useFetch('https://santosnr6.github.io/Data/events.json');
-   
-  if(isLoading) return <section className="page-text"><p>Loading...</p></section>;
-  if(isError) return <section className="page-text"><p>Error</p></section> 
-
-  if (!events) {
-   return <section className="page-text"><p>No events available.</p></section>
-  }
+function EventsList( {events} ) {
 
   return (
     <section className="eventslist">
--- src/pages/eventspage/EventsPage.jsx
+++ src/pages/eventspage/EventsPage.jsx
@@ -2,15 +2,25 @@ import "./eventsPage.css";
 import SearchBar from "../../components/searchbar/SearchBar";
 import EventsList from "../../components/eventslist/EventsList";
 import NavBar from "../../components/navbar/Navbar";
+import useFetch from "../../hooks/useFetch";
 
 function EventsPage() {
+  const { data : events, isLoading, isError } = useFetch('https://santosnr6.github.io/Data/events.json');
+   
+  if(isLoading) return <section className="page-text"><p>Loading...</p></section>;
+  if(isError) return <section className="page-text"><p>Error</p></section> 
+
+  if (!events) {
+   return <section className="page-text"><p>No events available.</p></section>
+  }
+
   return (
     <section className="events-page">
       <header className="events-page__header">
         <h1 className="events-page__title">Events</h1>
       </header>
-      <SearchBar />
-      <EventsList />
+      <SearchBar events={events} />
+      <EventsList events={events} />
       <NavBar />
     </section>
   );
