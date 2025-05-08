17e9ae21862ffb1c45814d1a00453ed9e1149f42
Mari Myrmoen <mari.myr@gmail.com>
2025-05-08 10:25:15 +0200

    Changed logic for adding new orders, had errors before

--- src/utils/generateSeats.js
+++ src/utils/generateSeats.js
@@ -2,7 +2,7 @@ function generateRandomSeat(quantity) {
   const sections = ["A", "B", "C", "D", "E"];
   const randomIndex = Math.floor(Math.random() * sections.length);
   const section = sections[randomIndex];
-  const startSeat = Math.floor(Math.random() * (200 - quantity)) + 1;
+  const startSeat = Math.floor(Math.random() * (300 - quantity)) + 1;
   const seats = Array.from({ length: quantity }, (_, i) => startSeat + i);
   return { section, seats };
 }
